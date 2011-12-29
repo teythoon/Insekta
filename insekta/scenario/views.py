3a2f09a419baf8fe8a6615fc4580b666eb6e0d75
Henning Pridöhl <henning@hprid.de>
2011-12-29 23:57:50 +0100

    Started scenario show view

--- insekta/scenario/views.py
+++ insekta/scenario/views.py
@@ -1,5 +1,10 @@
+from django.shortcuts import get_object_or_404
 from django.template.response import TemplateResponse
+from django.core.urlresolvers import reverse
 from django.contrib.auth.decorators import login_required
+
+from insekta.scenario.models import Scenario
+from insekta.scenario.creole import render_scenario
 
 @login_required
 def scenario_overview(request):
@@ -16,8 +21,19 @@ def all_scenarios(request):
     })
 
 @login_required
-def show_scenario(request):
+def show_scenario(request, scenario_name):
     """Shows the description of a scenario."""
-    return TemplateResponse(request, 'scenario/show.html', {
+    scenario = get_object_or_404(Scenario, name=scenario_name)
 
+    target_url = reverse('scenario.show', args=(scenario_name, ))
+    environ = {
+        'vm_target': target_url,
+        'vm_state': 'disabled', # FIXME: Read current status
+        'user': request.user,
+        'enter_secret_target': target_url,
+        'submitted_secrets': scenario.get_submitted_secrets(request.user)
+
+    }
+    return TemplateResponse(request, 'scenario/show.html', {
+        'description': render_scenario(scenario.description, environ=environ),
     })
