daf67f6a816c4e6c7d7c224019cd65f0f7f7280a
Henning Pridöhl <henning@hprid.de>
2012-01-13 15:34:29 +0100

    Better documentation for network utility functions

--- insekta/network/utils.py
+++ insekta/network/utils.py
@@ -1,12 +1,12 @@
 def ip_to_int(ip):
-    """Packs an IP into an integer, e.g. '127.0.0.1' into 2130706433"""
+    """Pack an IP into an integer, e.g. '127.0.0.1' into 2130706433"""
     ip_int = 0
     for octet in ip.split('.'):
         ip_int = (ip_int << 8) | int(octet, 10)
     return ip_int
 
 def int_to_ip(ip_int):
-    """Converts an integer into an IP, e.g. 2130706433 to '127.0.0.1'"""
+    """Convert an integer into an IP, e.g. 2130706433 to '127.0.0.1'"""
     octets = [0] * 4
     for i in xrange(4):
         octets[3 - i] = str(ip_int & 0xff)
@@ -14,6 +14,7 @@ def int_to_ip(ip_int):
     return '.'.join(octets)
 
 def cidr_to_netmask(cidr):
+    """Convert a cidr to a netmask, e.g. /28 (as int) to 255.255.255.240."""
     return 0xffffffff ^ (1 << 32 - cidr) - 1
 
 def iterate_ips(blocks):
